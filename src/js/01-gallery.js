ba2e487ebe8c90357cfcf70985db902f53452f7d
Andrey Shyshko <andrey.shyshko84@gmail.com>
2021-12-27 01:08:46 +0200

    выполнил 3 задания в js файлах

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryImages = document.querySelector('.gallery');
+const galleryMarkup = createGalleryImagesMarkup(galleryItems);
+
+galleryImages.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+     </a>
+`;
+    })
+    .join('');
+}
+
+const lightboxOptions = {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const lightboxGallery = new SimpleLightbox('.gallery a', lightboxOptions);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function savedCurrentTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+  console.log(e.seconds);
+}
+
+const throttledSavedCurrentTime = throttle(savedCurrentTime, 1000);
+
+player.on('timeupdate', throttledSavedCurrentTime);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
